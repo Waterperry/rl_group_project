bc6d0843ae7ce4e933bafee3481da75b6e738d8c
tom <>
2023-04-20 17:16:06 +0100

    add optional convolutional layers to DqnAgent and modify code to use the CarRacing env.

--- DqnAgent.py
+++ DqnAgent.py
@@ -9,17 +9,17 @@ from keras.optimizers.legacy.adam import Adam
 from matplotlib import pyplot as plt
 from UniformReplayBuffer import UniformReplayBuffer, Experience
 
-
 class DqnAgent:
     def __init__(self,
-                 num_observations,  # size of the state |s|
+                 obs_shape,  # size of the state |s|
                  num_actions,  # number of actions in the environment
-                 alpha=1e-3,   # AdaM learning rate
+                 alpha=1e-3,  # AdaM learning rate
                  epsilon=0.1,  # random move probability
-                 gamma=0.99,   # discount factor
-                 qnet_layer_params=(128, 64),  # neuron counts for the fully-connected layers of the Q Network
+                 gamma=0.99,  # discount factor
+                 qnet_fc_layer_params=(128, 64),  # neuron counts for the fully-connected layers of the Q Network
+                 qnet_conv_layer_params=(32, 64, 128),  # filter counts for convolutional layers of the Q network
                  rng_seed: int = None,  # seed to RNG (optional, for debugging really)
-                 debug: bool = False    # enable debugging mode, useful for stack traces in tensorflow functions
+                 debug: bool = False  # enable debugging mode, useful for stack traces in tensorflow functions
                  ):
         # public fields
 
@@ -32,9 +32,21 @@ class DqnAgent:
         self._debug = debug
 
         self._qnet = tf.keras.models.Sequential()
-        self._qnet.add(tf.keras.layers.InputLayer(input_shape=(num_observations,)))
-        for neuron_count in qnet_layer_params:
+        self._qnet.add(tf.keras.layers.InputLayer(input_shape=obs_shape))
+
+        # if we need to use convolutional layers, add them and pooling layers after.
+        if qnet_conv_layer_params is not None:
+            for filter_count in qnet_conv_layer_params:
+                self._qnet.add(tf.keras.layers.Conv2D(filter_count, kernel_size=3, activation='relu'))
+                self._qnet.add(tf.keras.layers.MaxPool2D())
+
+            self._qnet.add(tf.keras.layers.Flatten())
+
+        # add the fully-connected layers to the neural network.
+        for neuron_count in qnet_fc_layer_params:
             self._qnet.add(tf.keras.layers.Dense(neuron_count, 'relu'))
+
+        # add the final layer, with linear activation
         self._qnet.add(tf.keras.layers.Dense(num_actions, 'linear'))
 
         self._qnet.compile(Adam(learning_rate=alpha), loss=Huber(), run_eagerly=self._debug)
@@ -47,6 +59,10 @@ class DqnAgent:
         # choose a greedy action
         # generate action q values by calling the network on the current state.
         # qnet may expect a batched input, in which case we need to expand dims.
+
+        # the next line is a workaround because for some reason, CarRacing's observation is a tuple :(
+        if type(state) == tuple and len(state) == 2:
+            state = state[0]
         action_q_values = self._qnet(tf.expand_dims(state, axis=0))
         action_q_values = tf.squeeze(action_q_values)
 
@@ -107,9 +123,10 @@ def run_dqn_on_env(env: gym.Env, num_episodes=150, render=True, verbose=False):
 
     # reset the environment and get the initial state s0
     state = env.reset()
-
+    if len(state) == 2 and type(state) == tuple:
+        state = state[0]
     # create our DQN agent, passing it information about the environment's observation/action spec.
-    dqn_agent = DqnAgent(len(state), env.action_space.n, alpha=1e-4, epsilon=1e-4)
+    dqn_agent = DqnAgent(state.shape, env.action_space.n)
 
     replay_buffer = UniformReplayBuffer(max_length=10000, minibatch_size=32)
 
@@ -121,21 +138,28 @@ def run_dqn_on_env(env: gym.Env, num_episodes=150, render=True, verbose=False):
     for ep in range(num_episodes):
         ep_return = 0.
         state = env.reset()
+
         done = False
         while not done:
-            # call the action wrapper to get an e-greedy action
+            # state is a tuple in CarRacing for some reason. just get the pixel-based observation.
+            if len(state) == 2 and type(state) == tuple:
+                state = state[0]
+                # call the action wrapper to get an e-greedy action
             action = dqn_agent.action(state)
 
             # run the action on the environment and get the new info
-            new_state, reward, done, info = env.step(action)
+            new_state, reward, truncated, done, info = env.step(action)
 
+            # state is a tuple in CarRacing for some reason. just get the pixel-based observation.
+            if len(state) == 2 and type(state) == tuple:
+                new_state = new_state[0]
             # add the experience to our replay buffer
             experience = Experience(state, done, action, reward, new_state)
             replay_buffer.add_experience(experience)
 
             # render the environment
             if render:
-                env.render('human')
+                env.render()
 
             # train on the experience
             if not done:
@@ -155,7 +179,7 @@ def run_dqn_on_env(env: gym.Env, num_episodes=150, render=True, verbose=False):
         print(f"Episode {ep} over. Total return: {ep_return}")
 
     plt.plot(returns)
-    _ = plt.title("Agent total returns per episode"), plt.xlabel("Episode"), plt.ylabel("Return")
+    _ = plt.title("Agent total returns per episode (Training)"), plt.xlabel("Episode"), plt.ylabel("Return")
 
     plt.show()
 
@@ -199,25 +223,31 @@ def evaluate_agent_on_env(agent: DqnAgent, env: gym.Env, num_eval_episodes=100, 
 def main():
     # a really simple example observation and action space. Importantly, num_observations is the size of a state
     #    and num_actions is the number of actions available in the environment.
+
     example_state = (0., 0., 1.)
     example_action_set = list(range(5))
 
-    agent = DqnAgent(num_observations=len(example_state), num_actions=len(example_action_set), epsilon=1., rng_seed=150)
+    # we need to instruct the DQN Agent to not use convolutional layers, otherwise it will expect pixel-shaped inputs.
+    agent = DqnAgent(obs_shape=(3, ), num_actions=len(example_action_set), qnet_conv_layer_params=None,
+                     epsilon=1., rng_seed=150)
     print(f"Sampling a random action: {agent.action(example_state)}")
 
     agent.set_epsilon(0.)
     print(f"Sampling a greedy action: {agent.action(example_state)}")
 
     # train an agent on a given environment
-    test_env = gym.make('CartPole-v1')
-    trained_agent, returns = run_dqn_on_env(test_env, num_episodes=100, render=True)
+    test_env = gym.envs.make('CarRacing-v2', continuous=False, render_mode='human')
+
+    trained_agent, returns = run_dqn_on_env(test_env, num_episodes=1000, render=True)
 
     # evaluate the agent on the same environment
-    eval_returns = evaluate_agent_on_env(trained_agent, test_env, num_eval_episodes=10)
+    eval_returns = evaluate_agent_on_env(trained_agent, test_env, num_eval_episodes=250, render=False)
     plt.plot(eval_returns)
-    plt.title("Returns on evaluation environment"), plt.xlabel("Eval. episode"), plt.ylabel("Returns")
+    plt.title("Agent total returns per episode (Evaluation)"), plt.xlabel("Eval. episode"), plt.ylabel("Returns")
+    plt.show()
 
-    trained_agent.save_policy()
+    # trained_agent.save_policy()
+
 
 if __name__ == '__main__':
     main()
